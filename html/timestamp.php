e3e184a02d98fbf71279828a0b2f96a3c8172643
radiorob <radiorob@openbroadcaster.com>
2020-06-17 17:03:18 -0700

    gh-pages

--- html/timestamp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$filename = "../includes/alerts.json";
-if (file_exists($filename)) {
-    $time_stamp = gmdate ("F d Y H:i:s", filemtime($filename));
-    echo " Last NAAD Update: " . json_encode($time_stamp) . " UTC";
-}
-?>
